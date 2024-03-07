82deb2a5f6e18594f09c89fb106f400d6ffcdfc2
Professeur Cot <martin.couture@uqtr.ca>
2024-03-07 11:34:14 -0500

    Add files via upload
    Ajout d'une nouveauté

---
+++ unzipRep7zZip.py
@@ -0,0 +1,39 @@
+import os, py7zr, zipfile
+
+repertoire = r"C:\Users\Martin\Documents\Perso\Architecture de réseaux 1\H24\TP1Correction\Gr4"
+os.chdir(repertoire)
+
+
+def creationRepertoire(nomFichier):
+    print("Traitement du fichier: ", nomFichier)
+    os.makedirs(os.path.splitext(nomFichier)[0])
+
+
+def extraction7z(nomFichier):
+    creationRepertoire(nomFichier)
+    with py7zr.SevenZipFile(nomFichier, 'r') as archive:
+        archive.extractall(path=os.getcwd() + "/" + os.path.splitext(nomFichier)[0])
+
+
+def extractionZip(nomFichier):
+    creationRepertoire(nomFichier)
+    with zipfile.ZipFile(nomFichier, 'r') as zip:
+        zip.extractall(path=os.getcwd() + "/" + os.path.splitext(nomFichier)[0])
+
+fichiersRAR = []
+fichiers = os.listdir(repertoire)
+for fichier in fichiers:
+    if os.path.isfile(fichier):
+        if os.path.splitext(fichier)[1] == ".7z":
+            extraction7z(fichier)
+        elif os.path.splitext(fichier)[1] == ".zip":
+            extractionZip(fichier)
+        elif os.path.splitext(fichier)[1] == ".rar":
+            fichiersRAR.append(fichier)
+
+if len(fichiersRAR):
+    print("Les fichiers suivants n'ont pas été faits:")
+    for fichierRAR in fichiersRAR:
+        print(fichierRAR)
+
+
